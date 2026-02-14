36285692b3e3aaa728b626a532d5a19c4f9fb8bb
Leekiahn <kiahn2000@gmail.com>
2026-02-14 15:33:59 +0900

    feat: PoolManager 구현

--- Runtime/Pool/IPoolService.cs
+++ Runtime/Pool/IPoolService.cs
@@ -5,9 +5,11 @@ namespace PhikozzLibrary
 {
     public interface IPoolService
     {
+        void CreatePool<T>(T prefab, int defaultCapacity, int maxSize) where T : MonoBehaviour, IPoolObject;
         ObjectPool<T> Get<T>(T prefab) where T : MonoBehaviour, IPoolObject;
-        //void Release<T>(T obj) where T : MonoBehaviour, IPoolObject;
-        //void ReleaseAll<T>(T prefab) where T : MonoBehaviour, IPoolObject;
-        //void DestroyAll<T>(T prefab) where T : MonoBehaviour, IPoolObject;
+        void Release<T>(T prefab) where T : MonoBehaviour, IPoolObject;
+        void ReleaseAll<T>(T prefab) where T : MonoBehaviour, IPoolObject;
+        void DestroyAll<T>(T prefab) where T : MonoBehaviour, IPoolObject;
+        ObjectPool<T> GetPool<T>(T prefab) where T : MonoBehaviour, IPoolObject;
     }
 }
